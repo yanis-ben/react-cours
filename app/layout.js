20ea8631e82b84d54409302897086ac6e1cdf6a8
Ssouad <souad.djd.0@gmail.com>
2025-01-01 22:25:45 +0100

    modify package.json

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import clsx from "clsx";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
 const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: "ReactJourney",
