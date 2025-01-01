cef86781b1bc3ca484b1f1819298769352a6ced8
Ssouad <souad.djd.0@gmail.com>
2025-01-01 22:15:32 +0100

    Deploy to Vercel

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: "ReactJourney",
